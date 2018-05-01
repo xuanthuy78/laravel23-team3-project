b8dd61940d4fd2092f4157d36715cbb839874949
MINHTOAN <minhtoandut96@gmail.com>
2018-05-01 16:47:43 +0700

    Implement gloudemans/shoppingcart to project

--- config/app.php
+++ config/app.php
@@ -159,7 +159,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Gloudemans\Shoppingcart\ShoppingcartServiceProvider::class
+        Gloudemans\Shoppingcart\ShoppingcartServiceProvider::class,
 
     ],
 
